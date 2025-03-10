96c09f46364a2f24c37b482639d50725446224b7
Aditya Sharma <aditya.1910.sharma@gmail.com>
2025-03-10 18:43:12 +0000

    feat: Add maybe and adguard dns

--- backbone/postgres/scripts/seed.sql
+++ backbone/postgres/scripts/seed.sql
@@ -50,74 +50,25 @@ GRANT ALL ON SCHEMA public TO gotify;
 
 
 
--- firefly
-SELECT 'CREATE DATABASE firefly'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'firefly')\gexec
+-- maybe
+SELECT 'CREATE DATABASE maybe'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'maybe')\gexec
 
 DO
 $do$
 BEGIN
    IF EXISTS (
       SELECT FROM pg_catalog.pg_roles
-      WHERE  rolname = 'firefly') THEN
+      WHERE  rolname = 'maybe') THEN
 
-      RAISE NOTICE 'Role "firefly" already exists. Skipping.';
+      RAISE NOTICE 'Role "maybe" already exists. Skipping.';
    ELSE
-      CREATE ROLE firefly LOGIN PASSWORD 'firefly';
+      CREATE ROLE maybe LOGIN PASSWORD 'maybe';
    END IF;
 END
 $do$;
 
-\c firefly
-
-GRANT CONNECT ON DATABASE firefly TO firefly; 
-GRANT ALL ON SCHEMA public TO firefly;
-
-
--- pico
-SELECT 'CREATE DATABASE pico'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'pico')\gexec
-
-DO
-$do$
-BEGIN
-   IF EXISTS (
-      SELECT FROM pg_catalog.pg_roles
-      WHERE  rolname = 'pico') THEN
-
-      RAISE NOTICE 'Role "pico" already exists. Skipping.';
-   ELSE
-      CREATE ROLE pico LOGIN PASSWORD 'pico';
-   END IF;
-END
-$do$;
-
-\c pico
-
-GRANT CONNECT ON DATABASE pico TO pico; 
-GRANT ALL ON SCHEMA public TO pico;
-
-
-
--- dikodiko
-SELECT 'CREATE DATABASE dikodiko'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dikodiko')\gexec
-
-DO
-$do$
-BEGIN
-   IF EXISTS (
-      SELECT FROM pg_catalog.pg_roles
-      WHERE  rolname = 'dikodiko') THEN
-
-      RAISE NOTICE 'Role "dikodiko" already exists. Skipping.';
-   ELSE
-      CREATE ROLE dikodiko LOGIN PASSWORD 'dikodiko';
-   END IF;
-END
-$do$;
-
-\c dikodiko
-
-GRANT CONNECT ON DATABASE dikodiko TO dikodiko; 
-GRANT ALL ON SCHEMA public TO dikodiko;
+\c maybe
+GRANT CREATE ON DATABASE maybe TO maybe;
+GRANT CONNECT ON DATABASE maybe TO maybe; 
+GRANT ALL ON SCHEMA public TO maybe;
